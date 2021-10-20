e6dab3562355db645a43fe0a97f468f5950820a1
TomPechulis <tom_pechulis1@baylor.edu>
2021-10-20 17:32:26 -0500

    changed login

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,48 +1,45 @@
 import React, { Component } from 'react';
+
 /*
 The form which was previously present in the App component has been moved to its own separate component.
 */
 
 class Login extends Component {
-   constructor(props) {
-       super(props);
-       this.state = {name: '', number: '', professor: '', mode: '', semester: ["Fall", "Spring", "Summer"], message: '', level: ''};
-       this.handleInputChange = this.handleInputChange.bind(this);
-       this.handleChangeStatus = this.handleChangeStatus.bind(this);
-       this.handleSubmit = this.handleSubmit.bind(this);
-   }
-   handleChangeStatus(event) {
-       this.setState({level: event.target.value});
-   }
-   handleInputChange(event) {
-       const target = event.target;
-       const value = target.type === 'checkbox' ? target.checked : target.value;
-       const name = target.name;
-       this.setState({
-           [name]: value
-       });
-   }
-   handleSubmit(event) {
+    state = {
+        email: "",
+        password: ""
+    };
 
-     alert("Login Attempted")
-   }
-   componentDidMount() {
-   }
-   render() {
-       return (
+    handleEmail = event => {
+        this.setState({ email: event.target.value });
+    };
+
+    handlePassword = event => {
+        this.setState({ password: event.target.value });
+    };
+
+    render() {
+        return (
             <div>
-            <h1>Login Page</h1>
+                <br></br>
+                <label htmlFor="text-input">Enter Email <br></br></label>
+                <input onChange={this.handleEmail}
+                       placeholder="email"
 
-            <label>Email:</label>
-            <input name = "email" className="form-control" type="text"/><br></br>
-            <label>Password:</label>
-            <input name = "email" className="form-control" type="password"/>
-            <hr></hr>
-            <input name = "login" type="submit"></input>
-                
-            <div><small class="text-muted">Don't have an account? <a href="/register">Create One</a></small></div>
-           </div>
-           );
-   }
+                /><br></br>
+
+                <label htmlFor="password-input">Password <br></br></label>
+                <input
+                        type="password"
+                        placeholder="Password"
+                        id="password-input"
+                        onChange={this.handlePassword}
+                />
+
+            </div>
+
+        );
+    }
+
 }
 export default Login;
