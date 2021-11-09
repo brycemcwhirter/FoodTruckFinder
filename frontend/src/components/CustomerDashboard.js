7a29d30e71c121d4ebdca231b275ac7ac8b6d4d0
TomPechulis <tom_pechulis1@baylor.edu>
2021-11-09 14:26:54 -0600

    added google maps api to cust dash

--- frontend/src/components/CustomerDashboard.js
+++ frontend/src/components/CustomerDashboard.js
@@ -1,18 +1,38 @@
 import React, { Component } from 'react';
 import '../App.css';
 import NavbarLoggedIn from './NavBarLoggedIn';
+import {Map, GoogleApiWrapper, Marker} from "google-maps-react";
+
 /*
 The form which was previously present in the App component has been moved to its own separate component.
 */
-
+const mapStyles={
+    width:'50%',
+    height: '50%'
+};
 class CustomerDashboard extends Component {
-    
+    state = {
+        stores: [{lat: 31.548, lng: -97.125},
+            {latitude: 31.546, longitude: -97.120},
+            {latitude: 31.551, longitude: -97.118}]
+    }
+
    handleSubmit(event) {
         alert("Updated");
 
    }
    componentDidMount() {
    }
+   displayMarkers = () => {
+       return this.state.stores.map((store, index) => {
+           return <Marker key={index} id={index} position={{
+               lat: store.latitude,
+               lng: store.longitude
+           }}
+                          onClick={() => console.log("You clicked me!")} />
+       })
+   }
+
    render() {
        return (
             <div>
@@ -76,6 +96,16 @@ class CustomerDashboard extends Component {
             </tbody>
             </table></div>
             </div>
+                <div>
+                    <Map
+                        google={this.props.google}
+                        zoom={14}
+                        style={mapStyles}
+                        initialCenter={{lat: 31.548, lng: -97.125}}
+                    >
+                        {this.displayMarkers()}
+                    </Map>
+                </div>
             </header>
             </div>
             </div>
@@ -84,4 +114,6 @@ class CustomerDashboard extends Component {
            );
    }
 }
-export default CustomerDashboard;
+export default GoogleApiWrapper({
+    apiKey:('AIzaSyD6WCgqYOmICfM4d29CP4_LN65Wk-Q-k-A')
+})(CustomerDashboard);
