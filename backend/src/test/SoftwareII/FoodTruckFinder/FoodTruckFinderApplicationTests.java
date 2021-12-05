9c5f0f1f678ab40f1a98c4a5306538af0eb6e8ce
MKM <malik_mohamedali1@baylor.edu>
2021-12-05 16:43:02 -0600

    Added JUnit tests for recommendation truck algorithm

--- backend/src/test/SoftwareII/FoodTruckFinder/FoodTruckFinderApplicationTests.java
+++ backend/src/test/SoftwareII/FoodTruckFinder/FoodTruckFinderApplicationTests.java
@@ -8,6 +8,7 @@ import SoftwareII.FoodTruckFinder.Data.FoodTruck.FoodTruck;
 import SoftwareII.FoodTruckFinder.Data.FoodTruck.FoodTruckPrice;
 import SoftwareII.FoodTruckFinder.Data.FoodTruck.FoodTruckRepository;
 import SoftwareII.FoodTruckFinder.Data.FoodTruck.FoodTruckType;
+import SoftwareII.FoodTruckFinder.Data.FoodTruck.Services.SortFoodTrucks;
 import SoftwareII.FoodTruckFinder.Data.Review.Review;
 import SoftwareII.FoodTruckFinder.Data.Review.ReviewRepository;
 import SoftwareII.FoodTruckFinder.Data.Route.Route;
@@ -26,6 +27,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
@@ -145,4 +149,54 @@ class ReviewRepositoryTests {
 		log.warn((reviewRepository.findAll().toString()));
 	}
 
+}
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+class FoodTruckAlgorithmTest {
+	private static final Logger log = LoggerFactory.getLogger(FoodTruckAlgorithmTest.class);
+	@Autowired AccountRepository accountRepository;
+	@Autowired FoodTruckRepository foodTruckRepository;
+
+	@Test
+	public void RecommendedTrucksAlgorithmTest() {
+		List<FoodTruck> l = new ArrayList<FoodTruck>();
+		SortFoodTrucks sort = new SortFoodTrucks();
+
+		Account account = new Account("MalikM", "Malik_Mohamedali1@baylor.edu", "broskiwoski", AccountType.FOODTRUCKOWNER, FoodTruckType.MEXICAN, FoodTruckPrice.$$);
+		accountRepository.save(account);
+
+		FoodTruck t1 = new FoodTruck("t1", FoodTruckType.AMERICAN, "a", "Waco", "TX", "76798", 3, true, FoodTruckPrice.$$, account);
+		FoodTruck t2 = new FoodTruck("t2", FoodTruckType.ASIAN, "a", "Waco", "TX", "76798", 5, true, FoodTruckPrice.$$$, account);
+		FoodTruck t3 = new FoodTruck("t3", FoodTruckType.MEXICAN, "a", "Waco", "TX", "76798", 2, true, FoodTruckPrice.$$, account);
+		FoodTruck t4 = new FoodTruck("t4", FoodTruckType.DRINKS, "a", "Waco", "TX", "76798", 5, true, FoodTruckPrice.$$, account);
+		FoodTruck t5 = new FoodTruck("t5", FoodTruckType.MEXICAN, "a", "Waco", "TX", "76798", 4, true, FoodTruckPrice.$, account);
+		FoodTruck t6 = new FoodTruck("t1", FoodTruckType.GERMAN, "a", "Waco", "TX", "76798", -1, true, FoodTruckPrice.$$, account);
+		foodTruckRepository.save(t1);
+		foodTruckRepository.save(t2);
+		foodTruckRepository.save(t3);
+		foodTruckRepository.save(t4);
+		foodTruckRepository.save(t5);
+		foodTruckRepository.save(t6);
+
+		l.add(t1);
+		l.add(t2);
+		l.add(t3);
+		l.add(t4);
+		l.add(t5);
+		l.add(t6);
+
+		l = sort.sortRecommended(l, account.getTypePreference().toString(), account.getPricePreference().toString());
+		assertNotNull(l);
+
+		assertEquals(l.get(0), t3);
+		assertEquals(l.get(1), t4);
+		assertEquals(l.get(2), t5);
+		assertEquals(l.get(3), t1);
+		assertEquals(l.get(4), t2);
+		assertEquals(l.get(5), t6);
+
+		log.warn(l.toString());
+
+	}
 }
