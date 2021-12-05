07ab38be4b5ec9696a525414d196bdf6275a65f3
MKM <malik_mohamedali1@baylor.edu>
2021-12-05 16:17:56 -0600

    Added JUnit tests for review and route repos

--- backend/src/main/java/SoftwareII/FoodTruckFinder/Data/Account/AccountRepository.java
+++ backend/src/main/java/SoftwareII/FoodTruckFinder/Data/Account/AccountRepository.java
@@ -7,5 +7,4 @@ import org.springframework.stereotype.Repository;
 public interface AccountRepository extends JpaRepository <Account, Long>{
     Account findByUsername(String username);
     Account findByEmail(String email);
-
 }
--- backend/src/test/SoftwareII/FoodTruckFinder/FoodTruckFinderApplicationTests.java
+++ backend/src/test/SoftwareII/FoodTruckFinder/FoodTruckFinderApplicationTests.java
@@ -8,7 +8,9 @@ import SoftwareII.FoodTruckFinder.Data.FoodTruck.FoodTruck;
 import SoftwareII.FoodTruckFinder.Data.FoodTruck.FoodTruckPrice;
 import SoftwareII.FoodTruckFinder.Data.FoodTruck.FoodTruckRepository;
 import SoftwareII.FoodTruckFinder.Data.FoodTruck.FoodTruckType;
+import SoftwareII.FoodTruckFinder.Data.Review.Review;
 import SoftwareII.FoodTruckFinder.Data.Review.ReviewRepository;
+import SoftwareII.FoodTruckFinder.Data.Route.Route;
 import SoftwareII.FoodTruckFinder.Data.Route.RouteRepository;
 
 import static org.junit.Assert.assertEquals;
@@ -41,7 +43,7 @@ class AccountRepositoryTests {
 		Account accountPrime = accountRepository.findByUsername("SheldonS");
 		accountRepository.save(account2);
 
-		log.info(accountRepository.findAll().toString());
+		log.warn(accountRepository.findAll().toString());
 
 		assertNotNull(account);
 		assertEquals(accountPrime.getUsername(), account.getUsername());
@@ -65,7 +67,7 @@ class FoodTruckRepositoryTests {
 		Account account2 = new Account("MalikM", "Malik_Mohamedali1@baylor.edu", "broskiwoski", AccountType.FOODTRUCKOWNER, FoodTruckType.MEXICAN, FoodTruckPrice.$$);
 		accountRepository.save(account);
 		accountRepository.save(account2);
-		
+
 		FoodTruck truck = new FoodTruck("Chick-fil-A", FoodTruckType.AMERICAN, "1311 S 5th St", "Waco", "TX", "76798", 3, true, FoodTruckPrice.$, account);
 		foodTruckRepository.save(truck);
 		FoodTruck truck2 = new FoodTruck("Best Food Truck", FoodTruckType.AMERICAN, "1001 Speight Ave", "Waco", "TX", "76706", -1, true, FoodTruckPrice.$, account2);
@@ -73,11 +75,74 @@ class FoodTruckRepositoryTests {
 
 		FoodTruck truckPrime = foodTruckRepository.findByName(truck.getName());
 
-		log.info(foodTruckRepository.findAll().toString());
+		log.warn(foodTruckRepository.findAll().toString());
 
 		assertNotNull(account);
 		assertEquals(truck.getName(), truckPrime.getName());
 		assertEquals(truck.getAddress(), truckPrime.getAddress());
+	}
+
+}
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+class RouteRepositoryTests {
+	private static final Logger log = LoggerFactory.getLogger(RouteRepositoryTests.class);
+	@Autowired FoodTruckRepository foodTruckRepository;
+	@Autowired RouteRepository routeRepository;
+	@Autowired AccountRepository accountRepository;
+
+	@Test
+	public void saveRouteTest() {
+
+		routeRepository.deleteAll();
+		Account account = new Account("SheldonS", "sheldon_smith2@baylor.edu", "Password1!", AccountType.CUSTOMER, FoodTruckType.ASIAN, FoodTruckPrice.$);
+		Account account2 = new Account("MalikM", "Malik_Mohamedali1@baylor.edu", "broskiwoski", AccountType.FOODTRUCKOWNER, FoodTruckType.MEXICAN, FoodTruckPrice.$$);
+		accountRepository.save(account);
+		accountRepository.save(account2);
+
+		FoodTruck truck = new FoodTruck("Chick-fil-A", FoodTruckType.AMERICAN, "1311 S 5th St", "Waco", "TX", "76798", 3, true, FoodTruckPrice.$, account);
+		foodTruckRepository.save(truck);
+		FoodTruck truck2 = new FoodTruck("Best Food Truck", FoodTruckType.AMERICAN, "1001 Speight Ave", "Waco", "TX", "76706", -1, true, FoodTruckPrice.$, account2);
+		foodTruckRepository.save(truck2);
+
+		routeRepository.save(new Route(truck, 1, "31.548","-97.125"));
+		routeRepository.save(new Route(truck, 2, "31.546","-97.120"));
+		routeRepository.save(new Route(truck2, 1, "31.551","-97.118"));
+
+		assertNotNull(routeRepository);
+		log.warn(routeRepository.findAll().toString());
+	}
+
+}
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+class ReviewRepositoryTests {
+	private static final Logger log = LoggerFactory.getLogger(ReviewRepositoryTests.class);
+	@Autowired FoodTruckRepository foodTruckRepository;
+	@Autowired ReviewRepository reviewRepository;
+	@Autowired AccountRepository accountRepository;
+
+	@Test
+	public void saveReviewTest() {
+
+		reviewRepository.deleteAll();
+		Account account = new Account("SheldonS", "sheldon_smith2@baylor.edu", "Password1!", AccountType.CUSTOMER, FoodTruckType.ASIAN, FoodTruckPrice.$);
+		Account account2 = new Account("MalikM", "Malik_Mohamedali1@baylor.edu", "broskiwoski", AccountType.FOODTRUCKOWNER, FoodTruckType.MEXICAN, FoodTruckPrice.$$);
+		accountRepository.save(account);
+		accountRepository.save(account2);
+
+		FoodTruck truck = new FoodTruck("Chick-fil-A", FoodTruckType.AMERICAN, "1311 S 5th St", "Waco", "TX", "76798", 3, true, FoodTruckPrice.$, account);
+		foodTruckRepository.save(truck);
+		FoodTruck truck2 = new FoodTruck("Best Food Truck", FoodTruckType.AMERICAN, "1001 Speight Ave", "Waco", "TX", "76706", -1, true, FoodTruckPrice.$, account2);
+		foodTruckRepository.save(truck2);
+
+		reviewRepository.save(new Review(5, "Great food, great service", account, truck));
+		reviewRepository.save(new Review(3, "Okay food, service could be improved", account2, truck2));
+
+		assertNotNull(reviewRepository);
+		log.warn((reviewRepository.findAll().toString()));
 	}
 
 }
